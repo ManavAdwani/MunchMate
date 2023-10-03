e7676e36a9ca3525a95dfdedaff7c3595bddc243
ManavAdwani <manavadwani00@gmail.com>
2023-10-03 23:46:38 +0530

    Index page almost completed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MunchMate - Homepage</title>
+</head>
+
+<body>
+    @include('navbar.navbar')
+    <div class="container">
+        <h4>Best offers for you</h4>
+        <div class="mainOffers">
+            <div class="offer1">
+                <img src="{{asset('logo/offer.png')}}" alt="">
+            </div>
+            <div class="offer2">
+                <img src="{{asset('logo/offer2.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container mt-4">
+        <div class="inyourmind">
+            <h4>What's in your mind ?</h4>
+            <p><a href="">View All !</a></p>
+        </div>
+        <div class="mainMind">
+            <img src="{{asset('logo/Pizza.webp')}}" alt="">
+            <img src="{{asset('logo/Burger.webp')}}" alt="">
+            <img src="{{asset('logo/North_Indian_4.webp')}}" alt="">
+            <img src="{{asset('logo/Chinese.webp')}}" alt="">
+            <img src="{{asset('logo/Pav_Bhaji.webp')}}" alt="">
+            <img src="{{asset('logo/South_Indian_4.webp')}}" alt="">
+            <img src="{{asset('logo/Ice_Creams.webp')}}" alt="">
+            <img src="{{asset('logo/Cakes.webp')}}" alt="">
+            <img src="{{asset('logo/Momos.webp')}}" alt="">
+            <img src="{{asset('logo/briyani.webp')}}" alt="">
+            <img src="{{asset('logo/noodles.webp')}}" alt="noodles">
+        </div>
+    </div>
+    <div class="hr mt-5">
+        <hr style="width: 80%;">
+    </div>
+    <div class="container mt-5">
+        <div class="topchains-title">
+            <h4>Top restaurant chains in Ahmedabad </h4>
+            <p><a href="">View All !</a></p>
+        </div>
+        <div class="chains mt-4 mb-5">
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/9/18438909/8ac67f799c14cd3b586e18e48eaa00f7_o2_featured_v2.jpg?output-format=webp"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">The Pizza Flavour</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Pizza, Pasta, Italian, Fastfood
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/9/18438909/8ac67f799c14cd3b586e18e48eaa00f7_o2_featured_v2.jpg?output-format=webp"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">The Pizza Flavour</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Pizza, Pasta, Italian, Fastfood
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/7/110397/2011193460b6f9638a7d7c7942492b5f_o2_featured_v2.jpg?output-format=webp"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">The Chocolate Factory</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Cafe, Coffee, Chocolate, Cakes
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/3/113923/42257d80a58880cde1e9105e3df9677e_o2_featured_v2.jpg?output-format=webp"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">asharfi-kulfi</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Desserts, Ice Cream, Beverages, Shake
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/5/112945/bf823dde89048658755ccf16bd3360c8_o2_featured_v2.jpg"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">KFC</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Burger, Fast Food, Biryani, Desserts, Beverages
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+            <div class="first">
+                <div class="image">
+                    <img src="https://b.zmtcdn.com/data/pictures/chains/1/110381/b060dff55283459118c532b9947aec19_o2_featured_v2.jpg"
+                        alt="">
+                </div>
+                <div class="title mt-2">
+                    <h5 class="title">Subway India</h5>
+                    <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                </div>
+                <div class="parent-container">
+                    <div class="description">
+                        Healthy Food, Fast Food, Sandwich, Salad, Wraps, Beverages
+                    </div>
+                    <div class="priceforone">
+                        ₹400 for one
+                    </div>
+                </div>
+                <div class="time">
+                    20 min
+                </div>
+                <div class="ordernow mt-5" style="width: 100%">
+                    <button class="btn btn-warning">Order Now</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-4">
+        <div class="bestrestaurant">
+            <div class="bestrestaurant-title">
+                <h4>Best restaurant near you </h4>
+                <p><a href="">View All !</a></p>
+            </div>
+            <div class="mainbestrestaurant">
+                <div class="first">
+                    <div class="image">
+                        <img src="https://b.zmtcdn.com/data/pictures/chains/9/18438909/8ac67f799c14cd3b586e18e48eaa00f7_o2_featured_v2.jpg?output-format=webp"
+                        alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">The Pizza India</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                            Pizza, Pasta, Italian, Fastfood
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+                <div class="first">
+                    <div class="image">
+                        <img src="https://b.zmtcdn.com/data/pictures/chains/1/110381/b060dff55283459118c532b9947aec19_o2_featured_v2.jpg"
+                            alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">Subway India</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                            Healthy Food, Fast Food, Sandwich, Salad, Wraps, Beverages
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+                <div class="first">
+                    <div class="image">
+                        <img src="https://media.istockphoto.com/id/545286388/photo/chinese-food-blank-background.jpg?s=612x612&w=0&k=20&c=pqOIy07YKO5PlU5VxjscwTGRrrZ8PluKMUjSOz-II60="
+                            alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">Chinese Corner</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                            Chinese, Noodles, Manchurian, Hakka Noodles
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+                <div class="first">
+                    <div class="image">
+                        <img src="https://img.onmanorama.com/content/dam/mm/en/food/features/images/2021/5/8/ice-cream.jpg"
+                            alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">Shakti Ice Cream Palour</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                           Ice cream, shakes, sandwhich
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+                <div class="first">
+                    <div class="image">
+                        <img src="https://static.toiimg.com/photo/75122042.cms"
+                            alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">Pops Juice Centre</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                            Healthy Food, Fast Food, Sandwich, Salad, Wraps, Beverages
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+                <div class="first">
+                    <div class="image">
+                        <img src="https://restaurantindia.s3.ap-south-1.amazonaws.com/s3fs-public/content9434.jpg"
+                            alt="">
+                    </div>
+                    <div class="title mt-2">
+                        <h5 class="title">RelaxZone Cafe</h5>
+                        <div class="stars">4.3&nbsp;<span class="material-symbols-outlined">star</span></div>
+                    </div>
+                    <div class="parent-container">
+                        <div class="description">
+                            Coffee, Tea, Beverages,Sandwhich
+                        </div>
+                        <div class="priceforone">
+                            ₹400 for one
+                        </div>
+                    </div>
+                    <div class="time">
+                        <div class="location">Ahmedabad</div>
+                        20 min
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="hr mt-5">
+        <hr style="width: 80%;">
+    </div>
+    <div class="container">
+        <div class="mt-5"></div>
+    </div>
+    <div class="container">
+        <div class="triomain" style="margin-top: 50px">
+            <div class="trio-title">
+                <center><h3>Food delivery you can count on</h3></center>
+            </div>
+            <br>
+            <div class="maintrio" style="margin-top: 50px">
+                <div class="row-first">
+                    <div class="ogfirst">
+                        <h3>Choose what you want</h3>
+                        <p>Select food items from your favourite store anywhere, anytime.</p>
+                        <div class="firstimage">
+                            <img src="https://www.bloggingherway.com/wp-content/uploads/2022/02/haute-stock-photography-mompreneur-final-10-scaled-e1618025674267-1024x683.jpeg.webp" alt="">
+                        </div>
+                    </div>
+                    <div class="ogfirst">
+                        <h3>See real time updates</h3>
+                        <p>Personal shoppers pick items with care. Chat as they shop and manage your order</p>
+                        <div class="firstimage">
+                            <img src="https://hips.hearstapps.com/hmg-prod/images/how-to-check-screen-time-1669837476.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="ogfirst">
+                        <h3>Get your food delivered</h3>
+                        <p>Your food will be delivered within a minutes by our special delivery agents.</p>
+                        <div class="firstimage">
+                            <img src="https://img.freepik.com/premium-vector/online-food-delivery-service-your-home-courier-received-order-delivery-food-online-payment-was-successful-phone-pay-terminal-check-scooter-mapaddressvector-illustration_608021-1327.jpg?w=2000" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br>
+</body>
+
+</html>
