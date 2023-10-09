5c8bc0f3d02917db4a22872ebbf042d53af59fa4
ManavAdwani <manavadwani00@gmail.com>
2023-10-10 00:10:41 +0530

    5/10 Done

--- database/migrations/2023_10_09_182812_create_users_table.php
+++ database/migrations/2023_10_09_182812_create_users_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); 
+            $table->string('username'); 
+            $table->string('email'); 
+            $table->integer('phone_number'); 
+            $table->string('password'); 
+            $table->timestamps(); 
         });
     }
 
