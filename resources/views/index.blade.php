1b5a4cc5b1e4683998ea6ceee1ffa2b988910858
ManavAdwani <manavadwani00@gmail.com>
2023-10-10 22:53:30 +0530

    Day 6/30 Done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MunchMate - Homepage</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<body>
-    @include('navbar.navbar')
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>MunchMate - Homepage</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    
+    <body>
+        @include('navbar.navbar')
     <div class="container">
         <h4>Best offers for you</h4>
         <div class="mainOffers">
