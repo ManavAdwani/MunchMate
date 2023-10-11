8507794b621d84b6b948c49f4f8f307ac49d3ce4
Manav Adwani <133079896+ManavAdwani@users.noreply.github.com>
2023-10-11 10:59:47 +0530

    Restaurant Panel started

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,30 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('navbar', function(){
+Route::get('navbar', function () {
     return view('navbar.navbar');
 });
 
-Route::get('signIn',function(){
-    if(session()->get('username')){
+Route::get('restaurantNav',function(){
+    return view('navbar.RestaurantNav');
+});
+
+Route::get('signIn', function () {
+    if (session()->get('username')) {
         return redirect('/');
-    }else{
+    } else {
         return view('Sign in.sign_in');
     }
 })->name('sign_in');
 
-Route::post('signUp', [UserController::class,'signUp'])->name('signup');
-Route::post('login',[UserController::class,'login'])->name('login');
+Route::post('signUp', [UserController::class, 'signUp'])->name('signup');
+Route::post('login', [UserController::class, 'login'])->name('login');
+
+Route::get('Restaurant', function () {
+    if (session()->get('username')) {
+        return view('Restaurant.index');
+    }
+    else{
+        return view('Sign in.sign_in');
+    }
+})->name('restaurantIndex');
