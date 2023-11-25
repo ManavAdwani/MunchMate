523e44521887deb6ab436b8fe27a2f6c0f3e9624
Manav Adwani <133079896+ManavAdwani@users.noreply.github.com>
2023-11-25 16:28:09 +0530

    Cart errors solved

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -1,21 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Cart;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
-    public function index($userId,$restaurant_id){
-      $order = New order;
-      $order->user_id = $userId;
-      $order->restaurant_id= $restaurant_id;
-      $order->status = "Order Recieved";
-      $order->payment = "Online";
-      $order->notes = "";
-      $order->save();
-   return view('orderplaced');
-      
+  public function index($userId, $restaurant_id)
+  {
+    $order = new order;
+    $order->user_id = $userId;
+    $order->restaurant_id = $restaurant_id;
+    $order->status = "Order Recieved";
+    $order->payment = "Online";
+    $order->notes = "";
+    $order->save();
+    $findCart = Cart::where('user_id', $userId)->where('restaurant_id', $restaurant_id)->first();
+
+    if ($findCart) {
+      $findCart->delete();
     }
+
+    return view('orderplaced');
+  }
 }
