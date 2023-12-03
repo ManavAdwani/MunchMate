fb6132cc0efbc34ac7ca3f819ceefc2b8b03ca8a
ManavAdwani <manavadwani00@gmail.com>
2023-12-03 20:01:19 +0530

    MUN-3 Almost Completed just dish name is remaining

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,12 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\Address;
 use App\Models\Cart;
 use App\Models\Order;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use DB;
 
 class OrdersController extends Controller
 {
-  public function index($userId, $restaurantId)
+  public function index(Request $request, $userId, $restaurantId)
   {
+    $cartIds = $request->input('cartIds');
+    $cartToDb = [];
+    foreach ($cartIds as $cartId) {
+      $cartToDb[] = $cartId;
+    };
     $order = new Order;
     $order->user_id = $userId;
     $order->restaurant_id = $restaurantId;
@@ -19,8 +26,22 @@ class OrdersController extends Controller
     $order->notes = "";
     $order->address_id = 0;
     $order->grandTotal = session()->get('grandTotal');
+    $order->cart_id = implode(',', $cartToDb);
     $order->save();
     $orderId = $order->id;
     return view('Payment.index')->with('orderId', $orderId);
+  }
+
+  public function yourOrders()
+  {
+    $userId = session()->get('userId');
+    // dd($getdetails);
+    $getAllOrders = DB::table('orders')
+      ->select('orders.restaurant_id','restaurants.name', 'orders.id', 'orders.status', 'orders.grandTotal','orders.cart_id')
+      ->join('restaurants', 'restaurants.id', '=', 'orders.restaurant_id')
+      ->where('orders.user_id', $userId)
+      ->get();
+
+    return view('yourOrders.index', compact('getAllOrders'));
   }
 }
